d762d0cef613a90cab29e154b3f2a8ec20f78a67
Lebedko Dmitry <lebedko.dmitrii@gmail.com>
2014-01-09 17:35:30 +0400

    file changing. refactored.

--- vfs.c
+++ vfs.c
@@ -13,52 +13,44 @@
 #define FILENAME_LENGTH 32
 #define FILE_NUMBER 32
 
-#define IS_NOT_ENOUGH_SPACE 1000
-#define NO_FREE_BLOCKS 1001
-#define NO_FREE_FILE_DESCR_SPACE 1002
-#define FILE_DOES_NOT_EXIST 1003
+#define E_NES 1000
+#define E_NFB 1001
+#define E_NFD 1002
+#define E_FNE 1003
 
-#define FS_FILE "fs"
-
-typedef struct block {	
-	char data[BLOCK_SIZE];
-} block_t;
+#define FS_FILE "/home/dmitry/VSU/OS/task3/fs"
 
 typedef struct file_descr {
 	char not_free;
-	char isFolder;
+	char is_folder;
 	int start;
-	int size;					//size in blocks = size/ BLOCK_SIZE
+	int size;
 	char name[FILENAME_LENGTH];
-} file_descr_t;
+} fd_t;
 
-typedef struct filesystem {
-	//{ block is free: nextBlock[i] == -2, block points to another block: nextBlock[i] == n, block contains end of file: nextBlock[i] == -1}
-	int nextBlock[BLOCK_NUMBER];//adjacent blocks
-	block_t blocks[BLOCK_NUMBER];//blocks 
-	file_descr_t fd[FILE_NUMBER];//fat table
-} filesystem_t;
+//block is free: next[i] == -2
+//block points to another block: next[i] == n
+//block contains end of file: next[i] == -1
 
-filesystem_t fs;
+typedef struct fs {
+	int next[BLOCK_NUMBER];
+	fd_t fd[FILE_NUMBER];
+} fs_t;
+
+fs_t fs;
 
 
 int init_fs() {
 	int i = 0;
-	//initialize fat table
 	for (i = 0; i < sizeof(fs.fd)/sizeof(fs.fd[0]); i++) {
 		fs.fd[i].not_free = 0;
-		fs.fd[i].isFolder = 0;
+		fs.fd[i].is_folder = 0;
 		memset(fs.fd[i].name, '\0', FILENAME_LENGTH);
 		fs.fd[i].start = -1;
 		fs.fd[i].size = 0;
 	}
-	//initialize next blocks
 	for (i = 0; i < BLOCK_NUMBER; i++) {
-		fs.nextBlock[i] = -2;
-	}
-	//initialize blocks
-	for (i = 0; i < BLOCK_NUMBER; i++) {
-		memset(fs.blocks[i].data, '\0', BLOCK_SIZE);
+		fs.next[i] = -2;
 	}
 	return 0;
 }
@@ -67,168 +59,152 @@ int load_fs() {
 	int i = 0;
 	init_fs();
 	FILE *pfile = fopen(FS_FILE,"r");
-	fread(fs.fd, sizeof(file_descr_t), FILE_NUMBER, pfile);
-	fread(fs.nextBlock, sizeof(int), BLOCK_NUMBER, pfile);
+	fread(fs.fd, sizeof(fd_t), FILE_NUMBER, pfile);
+	fread(fs.next, sizeof(int), BLOCK_NUMBER, pfile);
 	for (i = 0; i < BLOCK_NUMBER; i++)
-		if (fs.nextBlock[i] == 0)
-			fs.nextBlock[i] = -2;
+		if (fs.next[i] == 0)
+			fs.next[i] = -2;
 	fclose(pfile);
 	return 0;
 }
 
-int find_free_file_descr_place() {
+int find_free_fd_id() {
 	int i = 0;
 	for (i = 0; i < FILE_NUMBER; i++) {
 		if (!fs.fd[i].not_free)
 			return i;
 	}
-	return NO_FREE_FILE_DESCR_SPACE;
+	return E_NFD;
 }
 
 int find_free_block() {
 	int i = 0;
 	for (i = 0; i < BLOCK_NUMBER; i++) {
-		if (fs.nextBlock[i] == -2)
+		if (fs.next[i] == -2)
 			return i;
 	}
-	return NO_FREE_BLOCKS;
+	return E_NFB;
+}
+
+fd_t *get_fd_by_id(int id) {
+	return &fs.fd[id];
 }
 
 int delete_file(char *name) {
 	return 0;
 }
 
-file_descr_t *get_file_meta(const char *name) {
-	int i = 0;
-	for (i = 0; i < FILE_NUMBER; i++) {
-		if (strcmp(name, fs.fd[i].name) == 0) {
-			return &fs.fd[i];
-		}
-	}
-	return NULL;
-}
-
-int write_fd(file_descr_t *fd, int id) {
+int write_fd(fd_t *fd, int id) {
 	FILE *pfile = fopen(FS_FILE, "r+");
-	fseek(pfile, id * sizeof(file_descr_t), SEEK_SET);
-	fwrite(fd, 1, sizeof(file_descr_t), pfile);
+	fseek(pfile, id * sizeof(fd_t), SEEK_SET);
+	fwrite(fd, 1, sizeof(fd_t), pfile);
 	fclose(pfile);
 	return 0;
 }
 
-int _write(file_descr_t *fd, void *data, int size) {
+int write_data(fd_t *fd, void *data, int size) {
 	if (size == 0)
 		return 0;
 	int i = fd->start, j = 0, b = BLOCK_SIZE;
-	FILE *pfile = fopen("fs", "r+");
+	FILE *pfile = fopen(FS_FILE, "r+");
 	while (i != -1) {
 		b = (j * BLOCK_SIZE + BLOCK_SIZE) > size ? size % BLOCK_SIZE : b;
-		fseek(pfile, sizeof(file_descr_t) * FILE_NUMBER + sizeof(int) * BLOCK_NUMBER + i * BLOCK_SIZE, SEEK_SET);
+		fseek(pfile, sizeof(fd_t) * FILE_NUMBER + sizeof(int) * BLOCK_NUMBER + i * BLOCK_SIZE, SEEK_SET);
 		fwrite(data + j * BLOCK_SIZE, 1, 1 * b, pfile);
-		i = fs.nextBlock[i];
+		i = fs.next[i];
 		j++;
 	}
+	fd->size = size;
 	fclose(pfile);
 	return 0;
 }
 
-int write_precedence_vector(file_descr_t *fd) {
+int write_precedence_vector(fd_t *fd) {
 	FILE *pfile = fopen(FS_FILE, "r+");
 	int i = fd->start;
-	fseek(pfile, sizeof(file_descr_t) * FILE_NUMBER + i * sizeof(int), SEEK_SET);
-	fwrite(&fs.nextBlock[i], sizeof(int), 1, pfile);
+	fseek(pfile, sizeof(fd_t) * FILE_NUMBER + i * sizeof(int), SEEK_SET);
+	fwrite(&fs.next[i], sizeof(int), 1, pfile);
 	while (i != -1) {
-		//printf("%d \n", i);
-		i = fs.nextBlock[i];
-		fseek(pfile, sizeof(file_descr_t) * FILE_NUMBER + i * sizeof(int), SEEK_SET);
-		fwrite(&fs.nextBlock[i], sizeof(int), 1, pfile);
+		i = fs.next[i];
+		fseek(pfile, sizeof(fd_t) * FILE_NUMBER + i * sizeof(int), SEEK_SET);
+		fwrite(&fs.next[i], sizeof(int), 1, pfile);
 	}
 	fclose(pfile);
 	return 0;
 }
 
-int add_file(char *name, char *data, int size, char isFolder) {
-	int i = 0;
-	int previousBlock = -1;
-	int writtenBlocks = 0;
-	int currentBlock = 0;
-	int startBlock = 0;
-	int blocks = 0;
-	int lastBlockBytes = 0;
-	int file_descr_place = find_free_file_descr_place();
-	if (file_descr_place == NO_FREE_FILE_DESCR_SPACE)
-		return NO_FREE_FILE_DESCR_SPACE;
-	fs.fd[file_descr_place].not_free = 1;
-	fs.fd[file_descr_place].isFolder = isFolder;
-	fs.fd[file_descr_place].size = size;
-	strcpy(fs.fd[file_descr_place].name, name);
-	startBlock = find_free_block();
-	if (startBlock == NO_FREE_BLOCKS)
-		return NO_FREE_BLOCKS;
+int add_file(char *name, char *data, int size, char is_folder) {
+	fd_t *fd = NULL;
+	int start = 0, curr = 0, prev = -1, blocks = 0, written = 0, bytes_in_last_block = 0, i = 0;
+	int fd_id = find_free_fd_id();
+	if (fd_id == E_NFD)
+		return E_NFD;
+	fd = get_fd_by_id(fd_id);
+	fd->not_free = 1;
+	fd->is_folder = is_folder;
+	fd->size = size;
+	strcpy(fd->name, name);
+	start = find_free_block();
+	if (start == E_NFB)
+		return E_NFB;
 	//write to blocks
-	fs.fd[file_descr_place].start = startBlock;
+	fd->start = start;
 	blocks = size / BLOCK_SIZE;
-	lastBlockBytes = size % BLOCK_SIZE;
-	previousBlock = currentBlock = startBlock;
-	while (writtenBlocks != blocks) {
-		writtenBlocks++;
-		previousBlock = currentBlock;
-		fs.nextBlock[previousBlock] = -1;
+	bytes_in_last_block = size % BLOCK_SIZE;
+	prev = curr = start;
+	while (written != blocks) {
+		written++;
+		prev = curr;
+		fs.next[prev] = -1;
 		//find next block
-		currentBlock = find_free_block();
-		if (currentBlock == NO_FREE_BLOCKS) {
+		curr = find_free_block();
+		if (curr == E_NFB) {
 			delete_file(name);
-			return NO_FREE_BLOCKS;
+			return E_NFB;
 		}
-		if (writtenBlocks == blocks) {
-			//fs.nextBlock[previousBlock] = -1;
+		if (written == blocks)
 			break;
-		}
 		//set precedence for blocks
-		fs.nextBlock[previousBlock] = currentBlock;
+		fs.next[prev] = curr;
 	}
-	if (lastBlockBytes != 0) {
+	if (bytes_in_last_block != 0) {
 		if (blocks != 0)
-			fs.nextBlock[previousBlock] = currentBlock;
-		previousBlock = currentBlock;
-		//fs.nextBlock[previousBlock] = -1;
+			fs.next[prev] = curr;
+		prev = curr;
 	}
-	fs.nextBlock[previousBlock] = -1;
-	_write(&fs.fd[file_descr_place], data, size);
-	write_fd(&fs.fd[file_descr_place], file_descr_place);	
-	write_precedence_vector(&fs.fd[file_descr_place]);
-	return file_descr_place;
+	fs.next[prev] = -1;
+	write_data(fd, data, size);
+	write_fd(fd, fd_id);	
+	write_precedence_vector(fd);
+	return fd_id;
 }
 
 
 //--------------------------------------------------------
 
-int get_data(file_descr_t *fd, char **data1) {
+int get_data(fd_t *fd, char **buf) {
 	FILE *pfile = fopen(FS_FILE, "r");
 	char *data = NULL;
-	int currentBlock = 0;
+	int curr = 0;
 	int gotBlocks = 0;
 	int i = 0;
 	int bytes = 0;
 	if (fd == NULL)
 		return -1;
 	data = (char*)malloc(sizeof(char) * fd->size);
-	currentBlock = fd->start;
-	while (currentBlock != -1) {
+	curr = fd->start;
+	while (curr != -1) {
 		bytes = (fd->size - gotBlocks * BLOCK_SIZE) > BLOCK_SIZE ? BLOCK_SIZE : (fd->size - gotBlocks * BLOCK_SIZE);
-		fseek(pfile, sizeof(file_descr_t) * FILE_NUMBER + BLOCK_NUMBER * sizeof(int) + currentBlock * BLOCK_SIZE, SEEK_SET); 
+		fseek(pfile, sizeof(fd_t) * FILE_NUMBER + BLOCK_NUMBER * sizeof(int) + curr * BLOCK_SIZE, SEEK_SET); 
 		fread(data + gotBlocks * BLOCK_SIZE, 1, bytes, pfile);
 		gotBlocks++;
-		currentBlock = fs.nextBlock[currentBlock];
+		curr = fs.next[curr];
 	}
 	fclose(pfile);
-	*data1 = data;
+	*buf = data;
 	return fd->size;
 }
 
-file_descr_t *get_meta_by_id(int id) {
-	return &fs.fd[id];
-}
 
 int get_id(char *data, int size, char *filename) {
 	int i = 0;
@@ -238,19 +214,19 @@ int get_id(char *data, int size, char *filename) {
 	return -1;
 }
 
-int get_meta(const char *path, file_descr_t **fd) {
+int get_meta(const char *path, fd_t **fd) {
 	int size = 0;
-	file_descr_t *meta = NULL;
+	fd_t *meta = NULL;
 	int meta_id = -1;
 	int id = -1;
 	char filename[FILENAME_LENGTH], *data, *start, *ptr = path;
 	memset(filename, '\0', FILENAME_LENGTH);
 	if (strcmp(path, "/") == 0) {
-		*fd = get_meta_by_id(0);
+		*fd = get_fd_by_id(0);
 		return 0;
 	}
 	if (*ptr++ == '/')
-		meta = get_meta_by_id(0);
+		meta = get_fd_by_id(0);
 	else 
 		return NULL;
 	while ((ptr - path) != strlen(path)) {
@@ -264,7 +240,7 @@ int get_meta(const char *path, file_descr_t **fd) {
 		id = get_id(data, size, filename);
 		if (id == -1)
 			return -1;
-		meta = get_meta_by_id(id);
+		meta = get_fd_by_id(id);
 		memset(filename, '\0', FILENAME_LENGTH);
 		free(data);
 	}
@@ -279,7 +255,7 @@ int mkdir1(const char *path, int type) {
 	char *directory;
 	char *filename;
 	int *data, *mdata;
-	file_descr_t *meta;
+	fd_t *meta;
 	while (p = *ptr == '/' ? ptr : p, *ptr++ != '\0');
 	if ((p - path) != 0) {
 		directory = (char*)malloc(sizeof(char) * (p - path));
@@ -298,7 +274,7 @@ int mkdir1(const char *path, int type) {
 	memcpy(mdata, data, size);
 	fd = add_file(filename, NULL, 0, type);
 	mdata[size/sizeof(int)] = fd;
-	_write(meta, mdata, size + sizeof(int));
+	write_data(meta, mdata, size + sizeof(int));
 	meta->size = size + sizeof(int);
 	write_fd(meta, id);
 	free(data);
@@ -313,7 +289,7 @@ int remove_dir(const char *path) {
 	char *p = NULL, *ptr = path;
 	char *directory;
 	int *data, *mdata;
-	file_descr_t *directory_fd, *file_fd;
+	fd_t *directory_fd, *file_fd;
 	while (p = *ptr == '/' ? ptr : p, *ptr++ != '\0');
 	if ((p - path) != 0) {
 		directory = (char*)malloc(sizeof(char) * (p - path));
@@ -332,7 +308,7 @@ int remove_dir(const char *path) {
 	for (i = 0; i < size/sizeof(int); i++)
 		if (data[i] != file_id)
 			mdata[j++] = data[i];
-	_write(directory_fd, mdata, size - sizeof(int));
+	write_data(directory_fd, mdata, size - sizeof(int));
 	directory_fd->size = size - sizeof(int);
 	write_fd(directory_fd, directory_id);
 	free(data);
@@ -346,12 +322,12 @@ int create_clear_fs() {
 	int i = 0;
 	char *buf;
 	FILE *pfile;
-	pfile = fopen("fs", "w+");
+	pfile = fopen(FS_FILE, "w+");
 	//allocate place for file descriptors
-	buf = (char*)malloc(sizeof(file_descr_t));
-	memset(buf, '\0', sizeof(file_descr_t));
+	buf = (char*)malloc(sizeof(fd_t));
+	memset(buf, '\0', sizeof(fd_t));
 	for (i = 0; i < FILE_NUMBER; i++) {
-		fwrite(buf, sizeof(file_descr_t), 1, pfile);
+		fwrite(buf, sizeof(fd_t), 1, pfile);
 	}
 	free(buf);
 	//allocate place for precedence vector
@@ -362,10 +338,10 @@ int create_clear_fs() {
 	}
 	free(buf);
 	//allocate place for data blocks
-	buf = (char*)malloc(sizeof(block_t));
-	memset(buf, '\0', sizeof(block_t));
+	buf = (char*)malloc(BLOCK_SIZE);
+	memset(buf, '\0', BLOCK_SIZE);
 	for (i = 0; i < BLOCK_NUMBER; i++) {
-		fwrite(buf, sizeof(block_t), 1, pfile);
+		fwrite(buf, BLOCK_SIZE, 1, pfile);
 	}
 	free(buf);
 	fclose(pfile);
@@ -383,14 +359,14 @@ static int my_getattr(const char *path, struct stat *stbuf)
 
 	char *data;
 
-	file_descr_t *meta;
+	fd_t *meta;
 	int id = get_meta(path, &meta);
 
 	if (id == -1)
 		return -ENOENT;
 
 	memset(stbuf, 0, sizeof(struct stat));
-	if (meta->isFolder == 1) {
+	if (meta->is_folder == 1) {
 		stbuf->st_mode = S_IFDIR | 0755;
 		stbuf->st_nlink = 2;
 	} 
@@ -399,6 +375,7 @@ static int my_getattr(const char *path, struct stat *stbuf)
 		stbuf->st_nlink = 1;
 		stbuf->st_size = meta->size;
 	};
+	stbuf->st_mode = stbuf->st_mode | 0777;
 	return res;
 }
 
@@ -420,7 +397,7 @@ static int my_readdir(const char *path, void *buf, fuse_fill_dir_t filler, off_t
 	filler(buf, ".", NULL, 0);
 	filler(buf, "..", NULL, 0);
 
-	file_descr_t *meta;
+	fd_t *meta;
 	int id = get_meta(path, &meta);
 
 	if (id == -1)
@@ -438,16 +415,20 @@ static int my_readdir(const char *path, void *buf, fuse_fill_dir_t filler, off_t
 
 static int my_open(const char *path, struct fuse_file_info *fi)
 {
-	char *data;
-	file_descr_t *meta;
-	get_meta(path, &meta);
-	if (meta == NULL)
+	fd_t *meta;
+	int fd_id = 0;
+	fd_id = get_meta(path, &meta);
+	if (fd_id == -1)
 		return -ENOENT;
-	int len = get_data(meta, &data);
-	if (len < 0)
+	return 0;
+}
+
+static int my_opendir(const char *path, struct fuse_file_info *fi) {
+	fd_t *meta;
+	int fd_id = 0;
+	fd_id = get_meta(path, &meta);
+	if (fd_id == -1)
 		return -ENOENT;
-	if ((fi->flags & 3) != O_RDONLY)
-		return -EACCES;
 	return 0;
 }
 
@@ -459,7 +440,7 @@ static int my_mkdir(const char *path, mode_t mode) {
 	return 0;
 }
 
-static int my_unlink (const char *path) {
+static int my_unlink(const char *path) {
 	int res;
 	res = remove_dir(path);
 	if (res != 0)
@@ -467,13 +448,14 @@ static int my_unlink (const char *path) {
 	return 0;
 }
 
-static int my_write (const char *path, const char *buf, size_t size, off_t offset, struct fuse_file_info *fi) {
-	file_descr_t *fd;
+static int my_write(const char *path, const char *buf, size_t size, off_t offset, struct fuse_file_info *fi) {
+	fd_t *fd;
 	int res;
 	int fd_id = get_meta(path, &fd);
 	if (fd_id == -1)
 		return -ENOENT;
-	res = _write(fd, buf, size);
+	res = write_data(fd, buf, size);
+	write_fd(fd, fd_id);
 	if (res != 0)
 		return -1;
 	return 0;
@@ -492,7 +474,7 @@ static int my_read(const char *path, char *buf, size_t size, off_t offset,
 {
 	size_t len;
 	char *data;
-	file_descr_t *meta;
+	fd_t *meta;
 	get_meta(path, &meta);
 	if (meta == NULL)
 		return -ENOENT;
@@ -521,12 +503,20 @@ static struct fuse_operations hello_oper = {
 	.create		= my_create,
 	.unlink		= my_unlink,
 	.write		= my_write,
+	.opendir	= my_opendir,
 };
 
 int main(int argc, char *argv[])
 {
+	int i = 0;
 	if (argc > 1 && strcmp(argv[1],"n") == 0)
 		create_clear_fs();
 	load_fs();
+	printf("--------------------\n");
+	for (i = 0; i < FILE_NUMBER; i++)
+		printf("%s %d \n", fs.fd[i].name, fs.fd[i].start);
+	for (i = 0; i < BLOCK_NUMBER; i++)
+		printf("%d %d \n", i, fs.next[i]);
+	printf("--------------------\n");
 	return fuse_main(argc, argv, &hello_oper, NULL);
 }
